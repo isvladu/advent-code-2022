06db0a5be85044e2ed440893a53fb059a4bc51bd
isvladu <ionutsorin.vladu@gmail.com>
2022-12-13 12:14:53 +0200

    Day 12 OOP

--- day12.py
+++ day12.py
@@ -1,140 +1,109 @@
-from pprint import pprint
+from queue import Queue
 
 
-class Position:
-    def __init__(self, x: str, y: str, value: int):
-        self.x = x
-        self.y = y
-        self.value = value
+class Node:
+    def __init__(self, pos: tuple[int, int], elev: int) -> None:
+        self.pos = pos
+        self.elev = elev
+        self.paths = []
 
-    def __eq__(self, other):
-        if not isinstance(other, Position):
-            return NotImplemented
+    def add_path(self, path) -> None:
+        if path not in self.paths:
+            self.paths.append(path)
 
-        return self.x == other.x and self.y == other.y and self.value == other.value
+    def generate_paths(self, hmap) -> None:
+        x, y = self.pos
+        possible_paths = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
 
-    def __hash__(self):
-        return hash((self.x, self.y, self.value))
+        for path in possible_paths:
+            if path in hmap and hmap[path].elev <= self.elev + 1:
+                self.paths.append(path)
 
     def __str__(self):
-        return f"Position(x:{self.x},y:{self.y},value:{self.value})"
+        return f"Node(pos={self.pos},elev={self.elev})"
 
     def __repr__(self):
         return str(self)
 
 
-class InputParser:
-    def __init__(self, file: str) -> None:
-        self.file = file
-
-    def parse(self) -> tuple[list[list[str]], Position, Position]:
-        grid = []
-
-        with open(self.file, "r") as f:
-            for line in f:
-                grid.append(list(line.rstrip()))
-
-        for row_idx, row in enumerate(grid):
-            for col_idx, col in enumerate(row):
-                if col == "S":
-                    grid[row_idx][col_idx] = 0
-                    start = Position(row_idx, col_idx, 0)
-                elif col == "E":
-                    grid[row_idx][col_idx] = 25
-                    end = Position(row_idx, col_idx, 25)
-                else:
-                    grid[row_idx][col_idx] = ord(col) - ord("a")
-
-        return grid, start, end
-
-
 class Solution:
-    def __init__(
-        self,
-        grid: list[list[str]],
-        start: Position,
-        end: Position,
-    ) -> None:
-        self.grid = grid
-        self.start = start
-        self.end = end
-        self.graph = {}
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
 
-    def create_graph_util(self, position: Position) -> None:
-        self.graph[position] = []
+    def bfs(self, start_pos: tuple[int, int]) -> tuple[bool, int]:
+        # https://www.redblobgames.com/pathfinding/a-star/introduction.html
+        # Simple breadth-first search (BFS)
 
-        if position.x - 1 >= 0 and (
-            position.value - 1 <= self.grid[position.x - 1][position.y] <= position.value + 1
-        ):
-            self.graph[position].append(
-                Position(
-                    position.x - 1, position.y, self.grid[position.x - 1][position.y]
-                )
-            )
+        frontier = Queue()
+        frontier.put(start_pos)
+        came_from = dict()
+        came_from[start_pos] = None
 
-        if position.x + 1 < len(self.grid) and (
-            position.value - 1 <= self.grid[position.x + 1][position.y] <= position.value + 1
-        ):
-            self.graph[position].append(
-                Position(
-                    position.x + 1, position.y, self.grid[position.x + 1][position.y]
-                )
-            )
+        while not frontier.empty():
+            current_pos = frontier.get()
 
-        if position.y - 1 >= 0 and (
-            position.value - 1 <= self.grid[position.x][position.y - 1] <= position.value + 1
-        ):
-            self.graph[position].append(
-                Position(
-                    position.x, position.y - 1, self.grid[position.x][position.y - 1]
-                )
-            )
+            if current_pos == self.end_pos:
+                break
 
-        if position.y + 1 < len(self.grid[0]) and (
-            position.value - 1 <= self.grid[position.x][position.y + 1] <= position.value + 1
-        ):
-            self.graph[position].append(
-                Position(
-                    position.x, position.y + 1, self.grid[position.x][position.y + 1]
-                )
-            )
+            for next_position in self.height_map[current_pos].paths:
+                if next_position not in came_from:
+                    frontier.put(next_position)
+                    came_from[next_position] = current_pos
 
-    def create_graph(self) -> None:
-        for row_idx, row in enumerate(self.grid):
-            for col_idx, col in enumerate(row):
-                self.create_graph_util(Position(row_idx, col_idx, col))
+        current_pos = self.end_pos
+        path = []
 
-    def bfs(self) -> int | None:
-        visited = []
-        queue = [[self.start]]
+        while current_pos != start_pos:
+            path.append(current_pos)
+            try:
+                current_pos = came_from[current_pos]
+            except KeyError:
+                return False, came_from
 
-        if self.start == self.end:
-            return 0
+        return True, len(path)
 
-        while queue:
-            path = queue.pop(0)
-            node = path[-1]
+    def parse(self) -> None:
+        with open(self.filename, "r") as f:
+            grid = [line.rstrip() for line in f]
 
-            if node not in visited:
-                neighbours = self.graph[node]
+        self.height_map = {}
 
-                for neighbour in neighbours:
-                    new_path = list(path)
-                    new_path.append(neighbour)
-                    queue.append(new_path)
+        for y, row in enumerate(grid):
+            for x, c in enumerate(row):
+                if c == "S":
+                    self.start_pos = (x, y)
+                    elevation = 0
+                elif c == "E":
+                    self.end_pos = (x, y)
+                    elevation = 25
+                else:
+                    elevation = ord(c) - 97
+                self.height_map[(x, y)] = Node((x, y), elevation)
 
-                    if neighbour == self.end:
-                        return len(new_path)
+        for height in self.height_map:
+            self.height_map[height].generate_paths(self.height_map)
 
-                visited.append(node)
+    def solve(self) -> tuple[int, list[int]]:
+        part_2_paths = []
+        visited = set()
 
-        print(f"{node} - {new_path}")
-        return None
+        for coord in self.height_map:
+            if coord not in visited:
+                if self.height_map[coord].elev == 0:
+                    found, result = self.bfs(coord)
+                    if found:
+                        part_2_paths.append(result)
+                    else:
+                        for c in result:
+                            visited.add(c)
+                    if coord == self.start_pos:
+                        part_1 = result
+
+        return part_1, part_2_paths
 
 
 if __name__ == "__main__":
-    grid, start, end = InputParser("day12_2.in").parse()
-    solution = Solution(grid, start, end)
-    solution.create_graph()
-    # pprint(solution.graph)
-    print(solution.bfs() - 1)
+    solution = Solution("day12_2.in")
+    solution.parse()
+    part_1, part_2 = solution.solve()
+    print(f"Part 1: {part_1}, Part 2: {min(part_2)}")
